f918fc6911e6a2f9d309bcbea4727876bfd10b00
weslley lima <weslleyclima@yahoo.com.br>
2021-06-21 09:03:34 -0300

    Primeiro commit

---
+++ src/main/java/br/com/onedigitalinnovation/DesafioTestOfBeerApplication.java
@@ -0,0 +1,13 @@
+package br.com.onedigitalinnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTestOfBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTestOfBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/onedigitalinnovation/DesafioTestOfBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onedigitalinnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTestOfBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
